cf79b3873de76280b76edb0954577a37d86d01a8
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2021-08-26 18:32:05 -0300

    Utilizando async/await para melhorar o código

--- controller/listaClientes-controller.js
+++ controller/listaClientes-controller.js
@@ -7,7 +7,7 @@ const criaNovaLinha = (nome, email, id) => {
                     <td>${email}</td>
                     <td>
                         <ul class="tabela__botoes-controle">
-                            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                            <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
                             <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
                         </ul>
                     </td>
@@ -19,21 +19,21 @@ const criaNovaLinha = (nome, email, id) => {
 
 const tabela = document.querySelector('[data-tabela]')
 
-tabela.addEventListener('click', (evento) => {
+tabela.addEventListener('click', async (evento) => {
     let ehBotaoDeletar = evento.target.className === "botao-simples botao-simples--excluir"
-    if(ehBotaoDeletar){
+    if (ehBotaoDeletar) {
         const linhaCliente = evento.target.closest('[data-id]')
         let id = linhaCliente.dataset.id
-        clienteService.removeCliente(id)
-        .then( () => {
-            linhaCliente.remove()
-        })
+        await clienteService.removeCliente(id)
+        linhaCliente.remove()
     }
 })
 
-clienteService.listaClientes()
-    .then(data => {
-        data.forEach(elemento => {
+const render =  async () => {
+    const listaClientes = await clienteService.listaClientes()
+        listaClientes.forEach(elemento => {
             tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
         })
-    })
+}
+
+render()
